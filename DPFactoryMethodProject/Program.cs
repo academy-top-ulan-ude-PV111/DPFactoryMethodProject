2ec5ae484b0d0af77206cd5c6ad387a1d84ac5b5
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-08 16:23:39 +0300

    Добавьте файлы проекта.

---
+++ DPFactoryMethodProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPFactoryMethodProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory;
+            List<Factory> factories = new List<Factory>();
+            factories.Add(new InfantryFactory());
+            factories.Add(new ArcherFactory());
+            factories.Add(new CavaleryFactory());
+            factories.Add(new ElephantFactory());
+
+            Random random = new Random();
+
+            List<Unit> units = new List<Unit>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                factory = factories[random.Next(0, factories.Count)];
+                units.Add(factory.UnitCreate());
+            }
+
+            foreach (Unit unit in units)
+                unit.Info();
+        }
+    }
+}
---
+++ DPFactoryMethodProject/Unit.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPFactoryMethodProject
+{
+    abstract class Unit
+    {
+        public abstract void Info();
+    }
+
+    class Infantry : Unit
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Infantry");
+        }
+    }
+
+    class Cavalery : Unit
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Cavalery");
+        }
+    }
+
+    class Archer : Unit
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Archer");
+        }
+    }
+
+    class Elephant : Unit
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Elephant");
+        }
+    }
+
+    abstract class Factory
+    {
+        public abstract Unit UnitCreate();
+    }
+
+    class InfantryFactory : Factory
+    {
+        public override Unit UnitCreate()
+        {
+            return new Infantry();
+        }
+    }
+
+    class ArcherFactory : Factory
+    {
+        public override Unit UnitCreate()
+        {
+            return new Archer();
+        }
+    }
+
+    class CavaleryFactory : Factory
+    {
+        public override Unit UnitCreate()
+        {
+            return new Cavalery();
+        }
+    }
+    class ElephantFactory : Factory
+    {
+        public override Unit UnitCreate()
+        {
+            return new Elephant();
+        }
+    }
+
+}
